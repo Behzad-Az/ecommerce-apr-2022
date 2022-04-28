a34bf5d03a9cee7576cb8572f0ed56169afc353a
Behzad-Az <behy1368@gmail.com>
2022-04-27 20:05:35 -0700

    created navbar and footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,10 +1,17 @@
 import React, { FC } from 'react';
+import { AiFillInstagram, AiOutlineTwitter } from 'react-icons/ai';
 
 interface Props{};
 
 const Footer: FC<Props> = () : JSX.Element => {
   return (
-    <div>Footer</div>
+    <div className='footer-container'>
+      <p>2022 Dummy Store All rights reserved</p>
+      <p className='icons'>
+        <AiFillInstagram />
+        <AiOutlineTwitter />
+      </p>
+    </div>
   );
 };
 
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,24 @@
 import React, { FC } from 'react';
+import Link from 'next/link';
+import { AiOutlineShopping } from 'react-icons/ai';
 
 interface Props{};
 
 const Navbar: FC<Props> = () : JSX.Element => {
   return (
-    <div>Navbar</div>
+    <div className='navbar-container'>
+      <p className='logo'>
+        <Link href='/'>My Dummy Store</Link>
+      </p>
+      <button 
+        type='button' 
+        className='cart-icon'
+        onClick={() => null}
+      >
+        <AiOutlineShopping />
+        <span className='cart-item-qty'>1</span>
+      </button>
+    </div>
   );
 };
 
